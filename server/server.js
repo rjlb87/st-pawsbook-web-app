fa3fa6d18d88a2ad75f12624e24fac7fb41c2411
rjlb87 <rosellelabo.rl@gmail.com>
2023-04-03 16:33:53 +0800

    feature: added server endpoints

--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
-[ac]
+require('dotenv').config()
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const path = require('path')
+
+// Import Controllers
+// const ownersController = require('./controller/owners')
+
+// Initialize Express App
+const app = express()
+
+// Setup Port
+const port = process.env.DB_PORT || 3000
+
+// Express Setup
+app.use(express.static(path.join(__dirname, './frontend/build/')))
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+
+// API Endpoints
+app.get('/', (req, res) => {
+    res.send('Welcome to Awesome Application')
+})
+
+// Express Listening Port
+app.listen(port, () => {
+    console.log(`Server listening on the port: ${port}`)
+})
