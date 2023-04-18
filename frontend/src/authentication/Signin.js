1dd6098e9b46cd05c19857559bf37cf848b49e5e
rjlb87 <rosellelabo.rl@gmail.com>
2023-04-18 23:21:22 +0800

    updated sign in

--- frontend/src/authentication/Signin.js
+++ frontend/src/authentication/Signin.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Signin() {
     return (
-        <section class="bg-gray-50 dark:bg-gray-900 pt-40">
+        <div className="bg-gray-50 dark:bg-gray-900 pt-40">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -59,7 +59,7 @@ function Signin() {
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     )
 }
 
